03bfb31c54c5aaa5fae47fbb9152f825e2c0f5db
JedDizon <ku98sovk@students.codeinstitute.net>
2025-03-10 18:08:30 +0000

    linked to emailjs to send emails from website

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("service_ny44jpr","template_d0mb4ie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+    return false;
+}
